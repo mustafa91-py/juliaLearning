dc8e265c2628f92ac3d4001f16302aa25ac4bba7
Mustafa2 <Mustafauyar91@gmail.com>
2024-02-05 15:54:09 +0300

    if else bloklari

---
+++ conditionals.jl
@@ -0,0 +1,12 @@
+x = 10
+y = 5
+if x <y 
+    println("x küçüktür y den")
+elseif x == y 
+    println("x ve y eşittir")
+else
+    println("x büyüktür y den")
+    
+end
+## yukaridakinin kısa hali
+(x > y ? println(" x büyüktür y den") : println("x küçüktür yden"))
